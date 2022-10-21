34e5e7d1619bb351864caf151c2dd6a4febcf771
unknown <jameskagunga2022@gmail.com>
2022-10-21 04:11:25 -0700

    nodemailer

---
+++ nodemailer/src/Helpers/sendmail.ts
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+function createTransporter(config: any) {
+  const transporter = nodemailer.createTransport(config);
+  return transporter;
+}
+
+let configurations = {
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL as string,
+    pass: process.env.PASSWORD as string,
+  },
+};
+
+const sendMail = async (messageoption: any) => {
+  const transporter = await createTransporter(configurations);
+  await transporter.verify();
+  await transporter.sendMail(messageoption, (error, info) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log(info.response);
+  });
+};
+
+export default sendMail;
